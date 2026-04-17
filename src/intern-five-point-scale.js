a30d780548af2bebb3d5d22164e57b7210d7a293
airmanxtw <airmanx@stust.edu.tw>
2026-04-18 00:25:50 +0800

    重構五點量表元件，新增統計功能並移除不必要的檔案

---
+++ src/intern-five-point-scale.js
@@ -0,0 +1,101 @@
+import { LitElement, css, html } from 'lit'
+import litLogo from './assets/lit.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+/**
+ * An example element.
+ *
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+export class InternFivePointScale extends LitElement {
+  static styles = css`
+    table {
+      border-collapse: collapse;
+      width: 100%;
+    }
+
+    table,
+    th,
+    td {
+      border: 1px solid #000;
+      text-align: center;
+    }
+  `
+
+  static get properties() {
+    return {
+      /**
+       * The number of times the button has been clicked.
+       */
+      data: { type:Array },
+    }
+  }
+
+  constructor() {
+    super()
+    this.count = 0
+  }
+
+  render() {
+    return html`
+      <table>
+        <thead>
+          <tr>
+            <th rowspan="2">題目</th>
+            <th rowspan="2">問卷題目</th>
+            <th colspan="2">非常滿意</th>
+            <th colspan="2">滿意</th>
+            <th colspan="2">普通</th>
+            <th colspan="2">不滿意</th>
+            <th colspan="2">非常不滿意</th>
+            <th rowspan="2">平均分數</th>
+          </tr>
+          <tr>
+            <th>份數</th>
+            <th>百分比</th>
+            <th>份數</th>
+            <th>百分比</th>
+            <th>份數</th>
+            <th>百分比</th>
+            <th>份數</th>
+            <th>百分比</th>
+            <th>份數</th>
+            <th>百分比</th>
+          </tr>
+          
+        </thead>
+        <tbody>
+          ${this.data.map((item, index) => html`
+            <tr>
+              <td>${item.qno}</td>
+              <td>${item.qtext}</td>
+              <td>${item.fives.filter(f=>f==5).length}</td>
+              <td>${(item.fives.filter(f=>f==5).length/item.fives.length*100).toFixed(2)}%</td>
+              <td>${item.fives.filter(f=>f==4).length}</td>
+              <td>${(item.fives.filter(f=>f==4).length/item.fives.length*100).toFixed(2)}%</td>
+              <td>${item.fives.filter(f=>f==3).length}</td>
+              <td>${(item.fives.filter(f=>f==3).length/item.fives.length*100).toFixed(2)}%</td>
+              <td>${item.fives.filter(f=>f==2).length}</td>
+              <td>${(item.fives.filter(f=>f==2).length/item.fives.length*100).toFixed(2)}%</td>
+              <td>${item.fives.filter(f=>f==1).length}</td>
+              <td>${(item.fives.filter(f=>f==1).length/item.fives.length*100).toFixed(2)}%</td>
+              <td>${(item.fives.reduce((a,b)=>a+b,0)/item.fives.length).toFixed(2)}</td>
+            </tr>
+          `)}
+        </tbody>
+      </table>
+
+    `
+  }
+
+  _onClick() {
+    this.count++
+  }
+
+  
+  
+}
+
+window.customElements.define('intern-five-point-scale', InternFivePointScale)
